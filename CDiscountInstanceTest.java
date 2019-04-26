5e2fbba21dda58070cc201ff572f3c0db2f63f05
CacoRod <48563164+CacoRod@users.noreply.github.com>
2019-04-26 16:10:15 -0300

    Add files via upload

---
+++ CDiscountInstanceTest.java
@@ -0,0 +1,46 @@
+package ar.edu.ub.testing.Discount.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ar.edu.ub.testing.Discount.CDiscount;
+import ar.edu.ub.testing.Discount.CDiscountInstance;
+import ar.edu.ub.testing.Discount.CProductInstance;
+
+class CDiscountInstanceTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		CDiscountInstance CdisInsTest = new CDiscountInstance(CDiscTest, CProdInsTest, 0, "Test");
+		
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void discountTest() {
+		assertNotNull(CdisInsTest.discount());
+		assertEquals(CdisInsTest, CdisInsTest.discount());
+	}
+
+	void ProductInstanceTest() {
+		assertNotNull(CdisInsTest.productInstance());
+		assertEquals(CProdInsTest, CdisInsTest.productInstance());
+	}
+	
+	void ammountTest() {
+		assertNotNull(CdisInsTest.amount());
+		assertEquals(0, CdisInsTest.amount());
+	}
+	void nameTest() {
+		assertNotNull(CdisInsTest.name());
+		assertEquals("Test", CdisInsTest.name());
+	}
+	CDiscountInstance CdisInsTest;
+	CProductInstance CProdInsTest;
+	CDiscount CDiscTest;
+}
